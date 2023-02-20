071a1e0f3c1f2f58bd4e15679b834c7541bf1c5b
KevinArellano94 <Kevin.Arellano94@GMail.Com>
2023-02-20 18:48:07 -0500

    Add automation script that opens Visual Studio script.

---
+++ VistualStudioAdmin/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string visualStudioPath = @"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Visual Studio 2022.lnk";
+        
+        ProcessStartInfo startInfo = new ProcessStartInfo
+        {
+            FileName = visualStudioPath,
+            UseShellExecute = true,
+            Verb = "runas"
+        };
+        
+        try
+        {
+            Process.Start(startInfo);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
---
+++ VistualStudioAdmin/README.md
@@ -0,0 +1,23 @@
+# Automation - Visual Studio as Admin
+Runs Visual Studio as admin assuming it's installed in "C:\Program Files\~".
+
+## Features
+Under construction...
+
+## Getting started
+To run:
+```bash
+dotnet run
+```
+
+## Contributing
+Under construction...
+
+## Roadmap
+Under construction...
+
+## Reporting Security Issues
+Caveat: requires 'UseShellExecute' to be set as "true" for some reason.
+
+## License
+Licensed under the MIT License.
