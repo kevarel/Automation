c63e8fd700a482fe658ad92e89fc7ae58cf0a3b2
KevinArellano94 <Kevin.Arellano94@GMail.Com>
2023-02-22 19:51:30 -0500

    Add git commands project

---
+++ GitCommands/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ProcessStartInfo startInfo = new ProcessStartInfo();
+        startInfo.FileName = "git";
+        startInfo.Arguments = "branch";
+        startInfo.RedirectStandardOutput = true;
+
+        using (Process process = new Process())
+        {
+            process.StartInfo = startInfo;
+            process.Start();
+
+            string output = process.StandardOutput.ReadToEnd();
+            System.Console.WriteLine(output);
+        }
+    }
+}
---
+++ GitCommands/README.md
@@ -0,0 +1,28 @@
+﻿# Automation - Visual Studio as Admin
+Library git commands that works with other automation solutions.
+
+## Features
+Under construction...
+
+## Getting started
+To run:
+```bash
+dotnet run
+```
+
+To generate an executable:
+```bash
+dotnet build
+```
+
+## Contributing
+Under construction...
+
+## Roadmap
+Under construction...
+
+## Reporting Security Issues
+Caveat: requires 'UseShellExecute' to be set as "true" for some reason.
+
+## License
+Licensed under the MIT License.
